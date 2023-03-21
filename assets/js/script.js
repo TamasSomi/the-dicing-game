c0c78bdf0df0553507fd90664a528684ccdc211d
Tomi <somitamas1995@gmail.com>
2023-03-21 09:20:28 +0000

    Add function to set initial values

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ let rollDice = document.querySelector('.btn--roll-dice');
 let hold = document.querySelector('.btn--hold');
 let closeRules = document.querySelector('.btn-modal');
 
-let current, points, playing, activeplayer;
+let current, points, playing, activePlayer;
 
 // open rules with how to play button
 howToPlay.addEventListener('click', function(){
@@ -23,4 +23,21 @@ howToPlay.addEventListener('click', function(){
 closeRules.addEventListener('click', function(){
     document.querySelector('#modal').classList.toggle('hide-visibility');
 });
+
+// set starter values
+function setStarter() {
+    points = [0, 0];
+    playing = true;
+    current = 0;
+    activePlayer = 0;
+    elCurrent1.textContent = 0;
+    elCurrent2.textContent = 0;
+    elScore1.textContent = 0;
+    elScore2.textContent = 0;
+    document.querySelector('.dice').classList.add('hide-visibility');
+    document.querySelector('.won0').classList.add('hide-visibility');
+    document.querySelector('.won1').classList.add('hide-visibility');
+}
+
+setStarter();
 
