550a5656691b0db87c69fc1d6b2d69d3d244b294
Tomi <somitamas1995@gmail.com>
2023-03-21 08:54:25 +0000

    Select elements in js from html and put in variables

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let elPlayer1 = document.querySelector('#player0-name');
+let elPlayer2 = document.querySelector('#player1-name');
+let elScore1 = document.querySelector('#scr--0');
+let elScore2 = document.querySelector('#scr--1');
+let elCurrent1 = document.querySelector('#current--0');
+let elCurrent2 = document.querySelector('#current--1');
+let howToPlay = document.querySelector('.btn--how-to-play');
+let newGame = document.querySelector('.btn--new-game');
+let rollDice = document.querySelector('.btn--roll-dice');
+let hold = document.querySelector('.btn--hold');
