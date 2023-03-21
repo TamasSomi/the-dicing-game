f80613a5063d9a49caaff14db8185600d58138dc
Tomi <somitamas1995@gmail.com>
2023-03-21 09:10:01 +0000

    add variables for js values for points etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,14 @@ let rollDice = document.querySelector('.btn--roll-dice');
 let hold = document.querySelector('.btn--hold');
 let closeRules = document.querySelector('.btn-modal');
 
+let current, points, playing, activeplayer;
+
 // open rules with how to play button
 howToPlay.addEventListener('click', function(){
     document.querySelector('#modal').classList.toggle('hide-visibility');
 });
 
+// Close rules
 closeRules.addEventListener('click', function(){
     document.querySelector('#modal').classList.toggle('hide-visibility');
 });
